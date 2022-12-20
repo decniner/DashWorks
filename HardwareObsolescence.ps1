c7b656ae232cf8a7e4ff29aa81e872a9a41df681
Den <decniner@gmail.com>
2022-12-20 16:49:52 +0900

    Update HardwareObsolescence.ps1

--- HardwareObsolescence.ps1
+++ HardwareObsolescence.ps1
@@ -1,8 +1,15 @@
-if ($MachineModel -match "800G1" -or $MachineModel -match "800G2" -or $MachineModel -match "800G3" -or $MachineModel -match "800G4" -or $MachineModel -match "800G5") {
+# Define the list of compatible machine models
+$compatibleModels = "8138580G1", "805850G374", "80694830G4853", "375800G45849", "5859800G4845"
+
+# Check if the machine model is in the list of compatible models
+if ($compatibleModels -contains $MachineModel) {
+  # Check if the amount of memory is less than 16 GB
   if ($MemoryGB -lt 16) {
+    # Set the hardware compatibility to "Windows 10 supported"
     $HardwareCompatibility = "Windows 10 supported"
   }
   else {
-    $HardwareCompatibility = "Windows 11 Supported"
+    # Set the hardware compatibility to "Windows 11 supported"
+    $HardwareCompatibility = "Windows 11 supported"
   }
 }
